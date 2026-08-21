59017e3fbe91063f519fd03273f030e15c3829f0
g4B1n <gabingagno45@gmail.com>
2026-08-21 18:35:32 +0200

    feat(core): expose public runtime API

---
+++ packages/core/src/index.ts
@@ -0,0 +1,4 @@
+export { Fragment, createVNode, isVNode, normalizeChildren } from "./vnode.js";
+export type { Child, Component, PrimitiveChild, VNode, VNodeType } from "./vnode.js";
+export { batch, createComputed, createEffect, createSignal, isSignal, untrack } from "./reactivity.js";
+export type { Cleanup, Setter, Signal, Subscriber } from "./reactivity.js";
